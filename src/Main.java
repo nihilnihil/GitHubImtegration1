c199d458d826941054df8f970c0aae6fd8e655e2
Endre Beda <endre.beda@novaprospekt.at>
2022-05-18 17:36:35 +0200

    third and fourth line of source code add

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Second line of source code!");
+        System.out.println("Third line");
+        System.out.println("Fourth line");
     }
 }
